47a677bb4e1ffd2d283da7ce14c78b43945f2518
Данатей <danatey40793@gmail.com>
2021-04-25 19:53:00 +0300

    исправил 1таск

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,8 @@
 const searchCategories = document.querySelector('ul#categories');
 const quantityElements = searchCategories.children.length;
 console.log(`В списке ${quantityElements} категории.`);
-
-searchCategories.children.forEach(item => {
+const newArray = [...searchCategories.children];
+newArray.forEach((item) => {
     const searchTitle = item.querySelector('h2');
     const textElement = searchTitle.textContent;
     console.log(`Категория: ${textElement}`);
